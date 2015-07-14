3704d833abb2aa3fda2ebd28c1941c0918cb200c
Patrick Premont <patrick.premont@boldradius.com>
2015-07-14 09:44:12 -0400

    Relaxed input to sort to a Seq. Provided separate access to squaredDistance array building and sorting.

--- src/main/scala/com/boldradius/lambdarange/sort/SquaredDistanceSort.scala
+++ src/main/scala/com/boldradius/lambdarange/sort/SquaredDistanceSort.scala
@@ -2,14 +2,18 @@ package com.boldradius.lambdarange.sort
 import com.boldradius.lambdarange.Element
 import math.sqrt
 import com.boldradius.lambdarange.QuadNode.square
+import scala.collection.breakOut
 
 case class SquaredDistance[A](squaredDistance: Float, element: A) {
   def distance = sqrt(squaredDistance)
 }
 
 object SquaredDistanceSort {
-  def sortElements[A](elements: Array[Element[A]], center: (Float, Float)) : Array[SquaredDistance[Element[A]]] = {
-    val squaredDistances = elements.map(e => SquaredDistance(square(e.x - center._1) + square(e.y - center._2), e))
+  def elementSquaredDistances[A](elements: Seq[Element[A]], center: (Float, Float)) : Array[SquaredDistance[Element[A]]] =
+    elements.map[SquaredDistance[Element[A]], Array[SquaredDistance[Element[A]]]](e => SquaredDistance(square(e.x - center._1) + square(e.y - center._2), e))(breakOut)
+
+  def sortElements[A](elements: Seq[Element[A]], center: (Float, Float)) : Array[SquaredDistance[Element[A]]] = {
+    val squaredDistances = elementSquaredDistances(elements, center)
     java.util.Arrays.sort(squaredDistances, new Ordering[SquaredDistance[Element[A]]] {
       override def compare(x: SquaredDistance[Element[A]], y: SquaredDistance[Element[A]]): Int = {
         val xs = x.squaredDistance
